a8bdc0e4e477b9219178a04f47af6d0e2a34e8c9
alexhisen <ahisen@am.net>
2020-09-18 23:23:34 -0700

    Add new iOS icon sizes and remove redundant sizes that already exist Do not resize icon if target size is the same, just copy the file over - workaround for issue that was resulting in 10x increase in byte size for 1024px icon.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var _      = require('underscore');
 var Q      = require('q');
 var argv   = require('minimist')(process.argv.slice(2));
 
+var sourceIconSize;
+
 /**
  * @var {Object} settings - names of the config file and of the icon image
  */
@@ -14,8 +16,8 @@ var settings = {};
 settings.CONFIG_FILE = argv.config || 'config.xml';
 settings.ICON_FILE = argv.icon || 'icon.png';
 settings.OLD_XCODE_PATH = argv['xcode-old'] || false;
-settings.ICONS_PATH = argv['iconspath'] || false;
-settings.PLATFORMS = (argv['platforms'] || 'ios:android:osx:windows').split(':');
+settings.ICONS_PATH = argv.iconspath || false;
+settings.PLATFORMS = (argv.platforms || 'ios:android:osx:windows').split(':');
 
 /**
  * Check which platforms are added to the project and return their icon names and sizes
@@ -42,8 +44,10 @@ var getPlatforms = function (projectName) {
         { name: 'icon-20.png',             size : 20   },
         { name: 'icon-20@2x.png',          size : 40   },
         { name: 'icon-20@3x.png',          size : 60   },
-        { name: 'icon-40.png',             size : 40   },
+        { name: 'icon-24@2x.png',          size : 48   },
+        { name: 'icon-27.5@2x.png',        size : 55   },
         { name: 'icon-40@2x.png',          size : 80   },
+        { name: 'icon-44@2x.png',          size : 88   },
         { name: 'icon-50.png',             size : 50   },
         { name: 'icon-50@2x.png',          size : 100  },
         { name: 'icon-60@2x.png',          size : 120  },
@@ -53,20 +57,14 @@ var getPlatforms = function (projectName) {
         { name: 'icon-76.png',             size : 76   },
         { name: 'icon-76@2x.png',          size : 152  },
         { name: 'icon-83.5@2x.png',        size : 167  },
+        { name: 'icon-86@2x.png',          size : 172  },
+        { name: 'icon-98@2x.png',          size : 196  },
         { name: 'icon-1024.png',           size : 1024 },
         { name: 'icon-small.png',          size : 29   },
         { name: 'icon-small@2x.png',       size : 58   },
         { name: 'icon-small@3x.png',       size : 87   },
         { name: 'icon.png',                size : 57   },
         { name: 'icon@2x.png',             size : 114  },
-        { name: 'AppIcon24x24@2x.png',     size : 48   },
-        { name: 'AppIcon27.5x27.5@2x.png', size : 55   },
-        { name: 'AppIcon29x29@2x.png',     size : 58   },
-        { name: 'AppIcon29x29@3x.png',     size : 87   },
-        { name: 'AppIcon40x40@2x.png',     size : 80   },
-        { name: 'AppIcon44x44@2x.png',     size : 88   },
-        { name: 'AppIcon86x86@2x.png',     size : 172  },
-        { name: 'AppIcon98x98@2x.png',     size : 196  }
       ]
     });
   }
@@ -232,6 +230,16 @@ var generateIcon = function (platform, icon) {
   if (!fs.existsSync(dst)) {
     fs.mkdirsSync(dst);
   }
+
+  // There is some kind of issue in imagemagick where
+  // resizing to same size is making the png 10x larger in byte size.
+  if (icon.size === sourceIconSize) {
+    fs.copySync(srcPath, dstPath);
+    deferred.resolve();
+    display.success(icon.name + ' copied');
+    return deferred.promise;
+  }
+
   ig.resize({
     srcPath: srcPath,
     dstPath: dstPath,
@@ -365,10 +373,31 @@ var configFileExists = function () {
   return deferred.promise;
 };
 
+/**
+ * Get source icon size
+ *
+ * @return {Promise} resolves if no error reading it
+ */
+var getSourceIconSize = function () {
+  var deferred = Q.defer();
+  ig.identify(settings.ICON_FILE, function (err, info) {
+    if (err) {
+      display.error('cannot determine size of' + settings.ICON_FILE);
+      deferred.reject();
+    } else {
+      display.success(settings.ICON_FILE + ' info: ' + info);
+      sourceIconSize = info.width;
+      deferred.resolve();
+    }
+  });
+  return deferred.promise;
+};
+
 display.header('Checking Project & Icon');
 
 atLeastOnePlatformFound()
   .then(validIconExists)
+  .then(getSourceIconSize)
   .then(configFileExists)
   .then(getProjectName)
   .then(getPlatforms)
